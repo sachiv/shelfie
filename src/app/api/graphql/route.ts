b386d18eea3aacad22b8ca089a187eb1feccd0a2
Sachiv Paruchuri <sachivpvlns@gmail.com>
2025-06-05 19:39:44 +0530

    REvert temp

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -4,27 +4,18 @@ import "@/_lib/models/associations";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
-// Add this global cache
-declare global {
-  // eslint-disable-next-line no-var
-  var apolloServer: ApolloServer | undefined;
-}
-
-let server = globalThis.apolloServer;
-if (!server) {
-  server = globalThis.apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    formatError: (err) => {
-      console.error("GraphQL Error:", err);
-      return err;
-    },
-  });
-}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  formatError: (err) => {
+    console.error("GraphQL Error:", err);
+    return err;
+  },
+});
 
 // This wrapper ensures compatibility with Next.js app directory route expectations
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(request: Request, _context: unknown) {
-  // server is always defined here
-  return startServerAndCreateNextHandler(server!)(request);
+  // context is required by Next.js, but not used by Apollo
+  return startServerAndCreateNextHandler(server)(request);
 }
